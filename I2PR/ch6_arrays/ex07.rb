2312f4b974c0bc73c6ee82673980601b7f971cd3
Zach Ponder <zach.ponder@gmail.com>
2020-02-13 12:13:45 -0600

    Adding chapter 6

---
+++ I2PR/ch6_arrays/ex07.rb
@@ -0,0 +1,3 @@
+cities_of_alaska = ["Anchorage", "Fairbanks", "Jeneau", "Wasilla", "Sitka"]
+
+cities_of_alaska.each_with_index { |name, ndx| puts "#{ndx}: #{name}" }
---
+++ I2PR/ch6_arrays/ex08.rb
@@ -0,0 +1,6 @@
+arr_a = [1,2,4,8,16,32,64]
+
+arr_b = arr_a.map { |i| i + 2 }
+
+p arr_a
+p arr_b
